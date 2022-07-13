a8a59531fdaa08c531f8a225d0c03213abe5f784
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-07-14 02:16:32 +0530

    modified navigation bars

--- simplefrom/src/App.js
+++ simplefrom/src/App.js
@@ -23,15 +23,41 @@ function App() {
   return (
     <div>
       <div className="sidebar">
-        <nav className="Navs">
+        <nav>
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Review</li>
+            <li>
+              <div className="home-icon">
+                <div className="roof">
+                  <div className="roof-edge"></div>
+                </div>
+                <div className="front"></div>
+              </div>
+            </li>
+            <li>
+              <div className="about-icon">
+                <div className="head">
+                  <div className="eyes"></div>
+                  <div className="beard"></div>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="work-icon">
+                <div className="paper"></div>
+                <div className="lines"></div>
+                <div className="lines"></div>
+                <div className="lines"></div>
+              </div>
+            </li>
+            <li>
+              <div className="mail-icon">
+                <div className="mail-base">
+                  <div class="mail-top"></div>
+                </div>
+              </div>
+            </li>
           </ul>
         </nav>
-  </div>
       <div className="topbar">
         <nav className="navMenu">
           <a href="https://shayarimaza.com/14171-1/inspirational-guru-purnima-quotes">Home</a>
@@ -40,25 +66,26 @@ function App() {
           <a href="https://www.thefestivalwishes.com/p/write-name-on-guru-purnima-wishes-quotes-in-english">FAQs</a>
         </nav>
       </div>
-      <div className="container">
-        <div className="brand-title">JUST BLOGS...</div>
-        <div className="inputs">
-          <label>NAME</label>
-          <input value={form.Name} name="Name" onChange={(e) => handleChange(e)} type="text" placeholder="example@test.com" required />
-          <label>MOBILE</label>
-          <input value={form.Mobile} name="Mobile" onChange={(e) => handleChange(e)} type="number" placeholder="example@test.com" required />
-          <label>EMAIL</label>
-          <input value={form.Emails} name="Emails" onChange={(e) => handleChange(e)} type="email" placeholder="example@test.com" required />
-          <label>PASSWORD</label>
-          <input value={form.Password} name="Password" onChange={(e) => handleChange(e)} type="password" placeholder="Min 6 charaters long" required />
-          <label>CONFIRM-PASSWORD</label>
-          <input value={form.ConfirmPassword} name="ConfirmPassword" onChange={(e) => handleChange(e)} type="password" placeholder="Min 6 charaters long" required />
-          <button type="submit" onClick={handleSubmit}>LOGIN</button>
+        <div className="container">
+          <div className="brand-title">JUST BLOGS...</div>
+          <div className="inputs">
+            <label>NAME</label>
+            <input value={form.Name} name="Name" onChange={(e) => handleChange(e)} type="text" placeholder="example@test.com" required />
+            <label>MOBILE</label>
+            <input value={form.Mobile} name="Mobile" onChange={(e) => handleChange(e)} type="number" placeholder="example@test.com" required />
+            <label>EMAIL</label>
+            <input value={form.Emails} name="Emails" onChange={(e) => handleChange(e)} type="email" placeholder="example@test.com" required />
+            <label>PASSWORD</label>
+            <input value={form.Password} name="Password" onChange={(e) => handleChange(e)} type="password" placeholder="Min 6 charaters long" required />
+            <label>CONFIRM-PASSWORD</label>
+            <input value={form.ConfirmPassword} name="ConfirmPassword" onChange={(e) => handleChange(e)} type="password" placeholder="Min 6 charaters long" required />
+            <button type="submit" onClick={handleSubmit}>LOGIN</button>
+          </div>
+          <a href="https://learnings12654600.blogspot.com/2019/12/">MADE BY JAYESH</a>
         </div>
-        <a href="https://learnings12654600.blogspot.com/2019/12/">MADE BY JAYESH</a>
       </div>
-    </div>
-  );
+      </div>
+      );
 }
 
-export default App;
+      export default App;
