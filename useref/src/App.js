782053fef382a383037a906fc1dd3c701f7c9006
Jayesh Mishra <106644611+MishraJayesh@users.noreply.github.com>
2022-08-05 21:06:56 +0530

    Jayes | Adding Custom Hooks

--- useref/src/App.js
+++ useref/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Test from './customhooks/index.js';
+import Memoization from './customhooks2';
+import Counterapp from './customhooks2/main';
 
 function App() {
   return (
     <div className="App">
      < Test />
+     < Memoization />
+     < Counterapp />
     </div>
   );
 }
